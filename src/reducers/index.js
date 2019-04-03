4c9e36e08ee38ca894818a8f5bdadecdc06c0e3f
Karin Ohman <kaohma@gmail.com>
2019-04-02 20:09:54 -0600

    Add new reducers to icombineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from 'redux';
 import { eventsReducer } from './eventsReducer/eventsReducer';
+import { userReducer } from './userReducer/userReducer';
+import { wishlistReducer } from './wishlistReducer/wishlistReducer';
+import { attendingReducer } from './attendingReducer/attendingReducer';
 import { errorReducer } from './errorReducer/errorReducer';
 import { loadingReducer } from './loadingReducer/loadingReducer';
 
 export const rootReducer = combineReducers({
   events: eventsReducer,
+  user: userReducer,
+  wishlist: wishlistReducer,
+  attending: attendingReducer,
   loading: loadingReducer,
   error: errorReducer,
 });
