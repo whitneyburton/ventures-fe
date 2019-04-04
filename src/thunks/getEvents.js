2975e3cec7833b9c259f867c42ff8cdc8d3e646d
Whitney Burton <whitneyburton9@gmail.com>
2019-04-04 17:17:27 -0600

    Change start_date to month for readability
    Co-authored-by: Karin Ohman <kaohma@gmail.com>

--- src/thunks/getEvents.js
+++ src/thunks/getEvents.js
@@ -1,15 +1,15 @@
 import { setEvents, setLoading, setError } from '../actions';
 import { fetchData } from '../utils/api';
 
-export const getEvents = (limit = null, event_type = null, state = null, start_date = null) => {
+export const getEvents = (limit = null, event_type = null, state = null, month = null) => {
   return async (dispatch) => {
     try {
       dispatch(setLoading(true));
-      let path = (limit || event_type || state || start_date) ? '/?' : '/';
-      if (limit) path = path + `limit=${limit}`;
-      if (event_type) path = path + `event-type=${event_type}`;
-      if (state) path = path + `state=${state}`;
-      if (start_date) path = path + `start-date=${start_date}`;
+      let path = (limit || event_type || state || month) ? '?' : '';
+      if (limit) path = path + `&limit=${limit}`;
+      if (event_type) path = path + `&event-type=${event_type}`;
+      if (state) path = path + `&state=${state}`;
+      if (month) path = path + `&month=${month}`;
       const events = await fetchData('/events' + path, 'GET');
       dispatch(setEvents(events.data));
     } catch (error) {
