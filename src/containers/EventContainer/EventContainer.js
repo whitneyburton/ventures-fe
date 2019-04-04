73ced4565ab5d7f1e7044b3a8656571a344cc97e
Whitney Burton <whitneyburton9@gmail.com>
2019-04-03 21:20:16 -0600

    Add EventContainer placeholder

--- src/containers/EventContainer/EventContainer.js
+++ src/containers/EventContainer/EventContainer.js
@@ -1,1 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+export class EventContainer extends Component {
+  render() {
+    return (
+      <div className='EventContainer'>
+      </div>
+    )
+  }
+}
+
