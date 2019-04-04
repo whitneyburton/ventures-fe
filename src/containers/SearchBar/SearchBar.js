305e72d3eefa9ea3baa0cc0c2ece83b4b3b8d4f1
Whitney Burton <whitneyburton9@gmail.com>
2019-04-03 21:21:25 -0600

    Add SearchBar with handleChange functionality

--- src/containers/SearchBar/SearchBar.js
+++ src/containers/SearchBar/SearchBar.js
@@ -1,1 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import searchIcon from '../../images/search.svg';
+
+export class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchQuery: ''
+    };
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='SearchBar'>
+        <h3 className='searchbar-title'>SEARCH EVENTS</h3>
+        <div className='searchbar-container'>
+          <img src={searchIcon} alt='search icon' className='searchbar-icon' />
+          <input onChange={this.handleChange} name='searchQuery' placeholder='Search all events' className='searchbar-input' />
+        </div>
+      </div>
+    );
+  }
+}
