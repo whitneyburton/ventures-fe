6ce04d3f3d0f4391f5fea657b5b2d6cb5fa4c142
Whitney Burton <whitneyburton9@gmail.com>
2019-04-05 15:04:01 -0600

    Add Profile component, state hook, and connect to store

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import EventContainer from '../EventContainer/EventContainer';
+import { setUser } from '../../actions';
+import { mockUser } from '../../data/mockData';
 
 export const Profile = () => {
+  const [contentEditable, toggleEditable] = useState(false);
+  const buttonText = contentEditable ? 'Save' : 'Edit Profile';
 
   return (
-    <div></div>
-  )
-}
+    <div className="Profile">
+      <div className="user-intro-section">
+        <img
+          src={mockUser.image_url}
+          alt="user headshot"
+          className="user-photo"
+        />
+        <h1 className="user-name">{mockUser.name.toUpperCase()}</h1>
+      </div>
+      <div className="user-bio-section">
+        <p className="user-about-me">About Me</p>
+        <button
+          onClick={() => toggleEditable(!contentEditable)}
+          className="edit-profile-button"
+        >
+          {buttonText}
+        </button>
+        <p className="user-bio" contentEditable={contentEditable}>
+          {mockUser.bio}
+        </p>
+      </div>
+      <div className="view-tabs-section">
+        <button className="upcoming-button">Upcoming</button>
+        <button className="wishlist-button">Wishlist</button>
+        <button className="past-button">Past</button>
+      </div>
+      <EventContainer />
+    </div>
+  );
+};
 
-export default Profile;
+const mapDispatchToProps = dispatch => ({
+  setUser: user => dispatch(setUser(user))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Profile);
+
+Profile.propTypes = {
+  setUser: PropTypes.func
+};
