47840b64a3290f533c04c82ff05e785e19052cab
Karin Ohman <kaohma@gmail.com>
2019-04-04 19:25:19 -0600

    Add event card component and display, style cards

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export const NavBar = () => {
   return (
     <div className='NavBar'>
-      <h3 className='ventures-title'>VENTURES</h3>
+      <h2 className='ventures-title'>Ventures</h2>
       <NavLink to='/' className='home-button' />
       <NavLink to='/profile' className='profile-button' />
     </div>
