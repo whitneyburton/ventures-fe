202a4a5f529bb8a9454081c58b3f7a3f105c32d5
Karin Ohman <kaohma@gmail.com>
2019-04-04 19:25:19 -0600

    Add event card component and display, style cards

--- src/containers/EventContainer/EventContainer.js
+++ src/containers/EventContainer/EventContainer.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import EventCard from '../../components/EventCard/EventCard';
 
-export const EventContainer = () => {
+export const EventContainer = (props) => {
     return (
       <div className='EventContainer'>
+      {
+        props.events.map(event => 
+          <EventCard 
+            id={event.id}
+            name={event.attributes.name}
+            image={event.attributes.image_url}
+            city={event.attributes.city}
+            state={event.attributes.state}
+            date={event.attributes.start_date}
+          />
+        )
+      }
       </div>
     )
 }
 
-export default EventContainer;
+export const mapStateToProps = (state) => ({
+  events: state.events,
+})
+
+export default connect(mapStateToProps)(EventContainer);
 
