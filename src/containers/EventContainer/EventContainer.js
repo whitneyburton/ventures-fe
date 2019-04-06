50e4c70a0c10f766ac0ab68591a02fe96d8e5d81
Whitney Burton <whitneyburton9@gmail.com>
2019-04-06 13:33:53 -0600

    Style EventContainer depending on pathname

--- src/containers/EventContainer/EventContainer.js
+++ src/containers/EventContainer/EventContainer.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-export const EventContainer = () => {
+export const EventContainer = (props) => {
+  const { pathname } = props;
+  const styles = pathname.includes('profile') ? 'EventContainer-profile' : 'EventContainer-home';
     return (
-      <div className='EventContainer'>
+      <div className={styles} >
       </div>
     )
 }
