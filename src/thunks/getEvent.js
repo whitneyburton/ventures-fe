72227e90af466121feddbc3ffb24ee38c65f14b3
Karin Ohman <kaohma@gmail.com>
2019-04-06 15:41:37 -0600

    Create get single event thunk Co-authored-by: Whitney Burton <whitneyburton9@gmail.com>

---
+++ src/thunks/getEvent.js
@@ -0,0 +1,15 @@
+import { setLoading, setError } from '../actions';
+import { fetchData } from '../utils/api';
+
+export const getEvent = (eventId) => {
+  return async (dispatch) => {
+    try {
+      dispatch(setLoading(true));
+      const event = await fetchData(`/events/${eventId}`, 'GET');
+      return event.data.attributes;
+    } catch (error) {
+      dispatch(setError(error.message));
+    }
+    dispatch(setLoading(false));
+  }
+}
