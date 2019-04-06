dbb1b0e3ef432d1aac5a1f9737b8b415c7098b49
Whitney Burton <whitneyburton9@gmail.com>
2019-04-06 13:32:17 -0600

    Add slogan to NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,8 +5,9 @@ export const NavBar = () => {
   return (
     <div className='NavBar'>
       <h3 className='ventures-title'>VENTURES</h3>
+      <p className='ventures-slogan'>A place to find and track outdoor events that speak to you.</p>
       <NavLink to='/' className='home-button' />
-      <NavLink to='/profile' className='profile-button' />
+      <NavLink to='/profile/upcoming' className='profile-button' />
     </div>
   )
 }
