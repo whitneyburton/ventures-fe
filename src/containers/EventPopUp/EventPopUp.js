0415477ca438993d223e38b78446bda6079086c9
Karin Ohman <kaohma@gmail.com>
2019-04-06 13:34:46 -0600

    Styly event pop-up

--- src/containers/EventPopUp/EventPopUp.js
+++ src/containers/EventPopUp/EventPopUp.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { mockEvents } from '../../data/mockData';
+import { Link } from 'react-router-dom';
 
 export const EventPopUp = (props) => {
   const [event, setEvent] =  useState({});
@@ -17,20 +18,29 @@ export const EventPopUp = (props) => {
     const { name, city, state, event_type, price, start_date, end_date, description, event_url, image_url, video_url } = event.attributes;
     return (
       <div className='event-pop-up'>
-        <div className='image-container'><img src={image_url} alt='event photo' /></div>
+        <div className='image-container'>
+          <img src={image_url} alt='event photo' />
+        </div>
         <h2>{name}</h2>
         <h4>{city}, {state}</h4>
-        <h4>Dates: {start_date} - {end_date}</h4>
-        <h4>{event_type}</h4>
-        <h4>{price}</h4>
+        <h4>Dates: <span className='light-text'>
+          {start_date === end_date ? {start_date} : `${start_date} to ${end_date}`}
+        </span></h4>
+        <h4 className='event-type'>Event Type: <span className='light-text'>{event_type}</span></h4>
+        <h4>Price: <span className='light-text'>${price}</span></h4>
         <p>{description}</p>
-        <a href={event_url}>Event Website</a>
+        <a className='event-link' href={event_url}>Event Website</a>
+        <div className='button-div'>
+          <button className='wishlist-button'></button>
+          <button className='attending-button'></button>
+        </div>
+        <Link to='/' className='close-pop-up'></Link>
       </div>
     )
   } else {
     return(<div>Loading...</div>)
   }
-
+  
 }
 
 export default EventPopUp;
