a9daffcf158ffd45cb18ae6cfc00932b818b58fd
Whitney Burton <whitneyburton9@gmail.com>
2019-04-07 17:59:07 -0600

    Add user check in Profile

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -9,57 +9,63 @@ export const Profile = ({ location, user }) => {
   const [contentEditable, toggleEditable] = useState(false);
   const buttonText = contentEditable ? 'Save' : 'Edit Profile';
 
-  return (
-    <div className="Profile">
-      <div className="user-profile">
-        <img src={user.image_url} alt="user headshot" className="user-photo" />
-        <div className="user-bio-section">
-          <div className="bio-header">
-            <h1 className="user-name" contentEditable={contentEditable}>
-              {user.name.toUpperCase()}
-            </h1>
-            <button
-              onClick={() => toggleEditable(!contentEditable)}
-              className="edit-profile-button"
-            >
-              {buttonText}
-            </button>
+  if (user.name) {
+    return (
+      <div className="Profile">
+        <div className="user-profile">
+          <img src={user.image_url} alt="user headshot" className="user-photo" />
+          <div className="user-bio-section">
+            <div className="bio-header">
+              <h1 className="user-name" contentEditable={contentEditable}>
+                {user.name.toUpperCase()}
+              </h1>
+              <button
+                onClick={() => toggleEditable(!contentEditable)}
+                className="edit-profile-button"
+              >
+                {buttonText}
+              </button>
+            </div>
+            <p className="user-about-me">ABOUT ME</p>
+            <p className="user-bio" contentEditable={contentEditable}>
+              {user.bio}
+            </p>
           </div>
-          <p className="user-about-me">ABOUT ME</p>
-          <p className="user-bio" contentEditable={contentEditable}>
-            {user.bio}
-          </p>
         </div>
+        <div className="view-tabs-section">
+          <NavLink
+            exact
+            to="/profile/upcoming"
+            className="upcoming-button"
+            activeClassName="active"
+          >
+            Upcoming
+          </NavLink>
+          <NavLink
+            exact
+            to="/profile/wishlist"
+            className="wishlist-button"
+            activeClassName="active"
+          >
+            Wishlist
+          </NavLink>
+          <NavLink
+            exact
+            to="/profile/past"
+            className="past-button"
+            activeClassName="active"
+          >
+            Past
+          </NavLink>
+        </div>
+        <EventContainer pathname={location.pathname} />
       </div>
-      <div className="view-tabs-section">
-        <NavLink
-          exact
-          to="/profile/upcoming"
-          className="upcoming-button"
-          activeClassName="active"
-        >
-          Upcoming
-        </NavLink>
-        <NavLink
-          exact
-          to="/profile/wishlist"
-          className="wishlist-button"
-          activeClassName="active"
-        >
-          Wishlist
-        </NavLink>
-        <NavLink
-          exact
-          to="/profile/past"
-          className="past-button"
-          activeClassName="active"
-        >
-          Past
-        </NavLink>
-      </div>
-      <EventContainer pathname={location.pathname} />
-    </div>
-  );
+    );
+  } else {
+    return (
+      <h1>Loading...</h1>
+    )
+  }
 };
 
 export const mapStateToProps = state => ({
