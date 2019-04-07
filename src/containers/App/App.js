811561bb7a4c4a0066138abadc604b316243b340
Whitney Burton <whitneyburton9@gmail.com>
2019-04-07 15:20:27 -0600

    Add getUser to useEffect in App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -8,45 +8,58 @@ import NavBar from '../../components/NavBar/NavBar';
 import { withRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getEvents } from '../../thunks/getEvents';
+import { getUser } from '../../thunks/getUser';
 
-export const App = ({ getEvents, location }) => {
+export const App = ({ getEvents, getUser, location, isLoading, error }) => {
   useEffect(() => {
+    getUser('1');
     getEvents();
   }, []);
 
   return (
     <div className="App">
-      <NavBar />
-      <Switch>
-        <Route path="/profile" component={Profile} />
-        <Route
-          path="/"
-          render={props => (
-            <Fragment>
-              <div className="search-and-filters">
-                <SearchBar />
-                <Filters />
-              </div>
-              <EventContainer pathname={props.location.pathname} />
-            </Fragment>
-          )}
-        />
-      </Switch>
-      <Switch>
-        <Route path='/event/:id' component={EventPopUp} />
-        <Route path='/profile/event/:id' component={EventPopUp} />
-      </Switch>
+      {isLoading && <h1>Loading...</h1>}
+      {!isLoading && (
+        <Fragment>
+          <NavBar />
+          <Switch>
+            <Route path="/profile" component={Profile} />
+            <Route
+              path="/"
+              render={() => (
+                <Fragment>
+                  <div className="search-and-filters">
+                    <SearchBar />
+                    <Filters />
+                  </div>
+                  <EventContainer pathname={location.pathname} />
+                </Fragment>
+              )}
+            />
+          </Switch>
+          <Switch>
+            <Route path="/event/:id" component={EventPopUp} />
+            <Route path="/profile/event/:id" component={EventPopUp} />
+          </Switch>
+        </Fragment>
+      )}
     </div>
   );
 };
 
+export const mapStateToProps = state => ({
+  isLoading: state.isLoading,
+  error: state.error
+});
+
 export const mapDispatchToProps = dispatch => ({
-  getEvents: () => dispatch(getEvents())
+  getEvents: () => dispatch(getEvents()),
+  getUser: id => dispatch(getUser(id))
 });
 
 export default withRouter(
   connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(App)
 );
