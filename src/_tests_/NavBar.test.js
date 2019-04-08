cb8c686bc75ecf3209adc33a786bc263c2612b0a
Whitney Burton <whitneyburton9@gmail.com>
2019-04-08 15:29:41 -0600

    Add NavBar tests

--- src/_tests_/NavBar.test.js
+++ src/_tests_/NavBar.test.js
@@ -1,1 +1,15 @@
+import React from 'react';
 import { shallow } from 'enzyme';
+import NavBar from '../components/NavBar/NavBar';
+
+describe('NavBar', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavBar />)
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
