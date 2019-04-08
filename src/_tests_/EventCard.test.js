eeaf352f1a7eb71bb2d81f1595782d3310ccbaa1
Whitney Burton <whitneyburton9@gmail.com>
2019-04-08 14:54:49 -0600

    Add EventCard tests

--- src/_tests_/EventCard.test.js
+++ src/_tests_/EventCard.test.js
@@ -1,1 +1,21 @@
+import React from 'react';
 import { shallow } from 'enzyme';
+import { EventCard } from '../components/EventCard/EventCard';
+import { mockUser } from '../data/mockData';
+
+const mockProps = {
+  user: mockUser
+}
+
+describe('EventCard', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<EventCard {...mockProps} />)
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+})
