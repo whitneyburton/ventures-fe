7847219c3a62dac4e0f1494229a001e1f31cb28c
Karin Ohman <kaohma@gmail.com>
2019-04-08 15:05:55 -0600

    Update get events tests

--- src/thunks/getEvents.js
+++ src/thunks/getEvents.js
@@ -1,13 +1,12 @@
 import { setEvents, setLoading, setError } from '../actions';
 import { fetchData } from '../utils/api';
 
-export const getEvents = (limit = null, event_type = null, state = null, month = null) => {
+export const getEvents = (event_type = null, state = null, month = null) => {
   return async (dispatch) => {
     try {
       dispatch(setLoading(true));
-      let path = (limit || event_type || state || month) ? '?' : '';
-      if (limit) path = path + `&limit=${limit}`;
-      if (event_type) path = path + `&event-type=${event_type}`;
+      let path = (event_type || state || month) ? '?' : '';
+      if (event_type) path = path + `&event_type=${event_type}`;
       if (state) path = path + `&state=${state}`;
       if (month) path = path + `&month=${month}`;
       const events = await fetchData('/events' + path, 'GET');
