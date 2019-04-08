10aaff201e6da73d629be18e0b9a862cb21cec6b
Whitney Burton <whitneyburton9@gmail.com>
2019-04-08 15:26:51 -0600

    Add EventPopUp tests and add title to iframe

--- src/_tests_/EventPopUp.test.js
+++ src/_tests_/EventPopUp.test.js
@@ -1,1 +1,30 @@
+import React from 'react';
 import { shallow } from 'enzyme';
+import { EventPopUp, mapDispatchToProps } from '../containers/EventPopUp/EventPopUp';
+import { getEvent } from '../thunks/getEvent';
+
+const mockProps = {
+  getEvent: jest.fn()
+}
+
+describe('EventPopUp', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<EventPopUp {...mockProps} />)
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('mapDispatchToProps', () => {
+    it.skip('should call dispatch with the getEvent thunk', async () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = getEvent('1');
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.getEvent('1');
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+  });
+});
