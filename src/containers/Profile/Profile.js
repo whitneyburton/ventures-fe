db2d74a607a5e8214047b2266232531956fc1a17
Whitney Burton <whitneyburton9@gmail.com>
2019-04-08 12:48:02 -0600

    Add updateUser functionality to Profile

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -4,32 +4,48 @@ import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import EventContainer from '../EventContainer/EventContainer';
 import { setUser } from '../../actions';
+import { updateUser } from '../../thunks/updateUser';
 
-export const Profile = ({ location, user }) => {
-  const [contentEditable, toggleEditable] = useState(false);
-  const buttonText = contentEditable ? 'Save' : 'Edit Profile';
-
+export const Profile = ({ location, user, updateUser }) => {
   if (user.name) {
+    const [contentEditable, toggleEditable] = useState(false);
+    const [name, editName] = useState(user.name);
+    const [bio, editBio] = useState(user.bio);
+    const buttonText = contentEditable ? 'Save' : 'Edit Profile';
+
+    const saveUser = () => {
+      toggleEditable(!contentEditable);
+      contentEditable && updateUser({ name, bio });
+    };
+
     return (
       <div className="Profile">
         <div className="user-profile">
-          <img src={user.image_url} alt="user headshot" className="user-photo" />
+          <img
+            src={user.image_url}
+            alt="user headshot"
+            className="user-photo"
+          />
           <div className="user-bio-section">
             <div className="bio-header">
-              <h1 className="user-name" contentEditable={contentEditable}>
-                {user.name.toUpperCase()}
-              </h1>
+              {!contentEditable && (
+                <h1 className="user-name">{name.toUpperCase()}</h1>
+              )}
+              {contentEditable && (
+                <input className='edit-name-input' value={name.toUpperCase()} onChange={e => editName(e.target.value)} />
+              )}
               <button
-                onClick={() => toggleEditable(!contentEditable)}
+                onClick={() => saveUser()}
                 className="edit-profile-button"
               >
                 {buttonText}
               </button>
             </div>
             <p className="user-about-me">ABOUT ME</p>
-            <p className="user-bio" contentEditable={contentEditable}>
-              {user.bio}
-            </p>
+            {!contentEditable && <p className="user-bio">{bio}</p>}
+            {contentEditable && (
+              <input className='edit-bio-input' value={bio} onChange={e => editBio(e.target.value)} />
+            )}
           </div>
         </div>
         <div className="view-tabs-section">
@@ -62,9 +78,7 @@ export const Profile = ({ location, user }) => {
       </div>
     );
   } else {
-    return (
-      <h1>Loading...</h1>
-    )
+    return <h1>Loading...</h1>;
   }
 };
 
@@ -73,7 +87,8 @@ export const mapStateToProps = state => ({
 });
 
 export const mapDispatchToProps = dispatch => ({
-  setUser: user => dispatch(setUser(user))
+  setUser: user => dispatch(setUser(user)),
+  updateUser: user => dispatch(updateUser(user))
 });
 
 export default connect(
@@ -82,5 +97,6 @@ export default connect(
 )(Profile);
 
 Profile.propTypes = {
-  setUser: PropTypes.func
+  setUser: PropTypes.func,
+  user: PropTypes.object
 };
