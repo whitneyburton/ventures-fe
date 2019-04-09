65102fdd25172fbd0d626377b4e33f14cd53f673
Karin Ohman <kaohma@gmail.com>
2019-04-08 18:31:02 -0600

    Add search functionality to components

--- src/containers/EventContainer/EventContainer.js
+++ src/containers/EventContainer/EventContainer.js
@@ -3,34 +3,50 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import EventCard from '../../components/EventCard/EventCard';
 
-export const EventContainer = ({ pathname, events }) => {
+export const EventContainer = ({ pathname, events, searchText }) => {
   const styles = pathname.includes('profile') ? 'EventContainer profile' : 'EventContainer home';
-    return (
-      <div className={styles}>
+
+  const filterEvents = () => {
+    return events.filter(event => {
+      const { name, city, state } = event.attributes;
+      const text = name + ' ' + city + ' ' + state;
+      return text.toUpperCase().includes(searchText.toUpperCase())
+    })
+  };
+
+  const shownEvents = searchText.length ? filterEvents() : events;
+  
+  return (
+    <div className={styles}>
       {
-        events.map(event => 
-          <EventCard 
-            id={event.id}
-            key={event.id}
-            name={event.attributes.name}
-            image={event.attributes.image_url}
-            city={event.attributes.city}
-            state={event.attributes.state}
-            date={event.attributes.start_date}
-            pathname={pathname}
-          />
-        )
+        shownEvents.map(event => {
+          const { name, image_url, city, state, start_date } = event.attributes;
+          return (
+            <EventCard 
+              id={event.id}
+              key={event.id}
+              name={name}
+              image={image_url}
+              city={city}
+              state={state}
+              date={start_date}
+              pathname={pathname}
+            />
+          )
+        })
       }
-      </div>
-    )
+    </div>
+  )
 }
 
 export const mapStateToProps = (state) => ({
+  searchText: state.searchText,
   events: state.events,
 });
 
 export default connect(mapStateToProps)(EventContainer);
 
 EventContainer.propTypes = {
-  events: PropTypes.array
+  events: PropTypes.array,
+  searchText: PropTypes.string,
 }
