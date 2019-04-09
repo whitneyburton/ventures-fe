d16f46eb16c30c79dccf5d8407bf1cb5454b961d
Karin Ohman <kaohma@gmail.com>
2019-04-08 19:38:12 -0600

    Add filter functionality

--- src/containers/Filters/Filters.js
+++ src/containers/Filters/Filters.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { getEvents } from '../../thunks/getEvents';
 
-export const Filters = ({ events }) => {
+export const Filters = ({ events, getEvents }) => {
   const [eventType, updateType] = useState('');
   const [month, updateMonth] = useState('');
   const [state, updateState] = useState('');
+
+  useEffect(() => {
+    getEvents(eventType, month, state)
+  }, [eventType, month, state]);
 
   const setFilters = (filter) => {
     const uniqueFilters = events.reduce((acc, event) => {
@@ -22,13 +27,18 @@ export const Filters = ({ events }) => {
 
   const setMonthFilter = () => {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    return months.map((month, index) => {
-      const matchingEvents = events.filter(event => {
-        const eventMonth = event.attributes.start_date.split('-')[0];
-        return parseInt(eventMonth) === index + 1
+
+    if (month !== '') {
+      return (<option key={month} value={month}>{months[month-1]}</option>)
+    } else {
+      return months.map((month, index) => {
+        const matchingEvents = events.filter(event => {
+          const eventMonth = event.attributes.start_date.split('-')[0];
+          return parseInt(eventMonth) === index + 1
+        })
+        return (<option key={month} value={index+1}>{month} ({matchingEvents.length})</option>)
       })
-      return (<option key={month} value={month}>{month} ({matchingEvents.length})</option>)
-    })
+    }
   }
 
   const formatType = (item) => {
@@ -37,7 +47,7 @@ export const Filters = ({ events }) => {
     return itemArr.join('');
   }
 
-  if (events.length) {
+  if (events) {
     return (
       <div className="Filters">
         <h3 className="filters-title">FILTER EVENTS</h3>
@@ -46,7 +56,7 @@ export const Filters = ({ events }) => {
           value={eventType}
           onChange={(e) => updateType(e.target.value)}
         >
-          <option key='Type' value=''>Event Type</option>
+          <option key='Type' value=''>All Event Types</option>
           {setFilters('event_type')}
         </select>
         <select
@@ -54,7 +64,7 @@ export const Filters = ({ events }) => {
           value={month}
           onChange={(e) => updateMonth(e.target.value)}
         >
-          <option key='Month' value=''>Month</option>
+          <option key='Month' value=''>All Months</option>
           {setMonthFilter()}
         </select>
         <select
@@ -62,7 +72,7 @@ export const Filters = ({ events }) => {
           value={state}
           onChange={(e) => updateState(e.target.value)}
         >
-          <option key='State'value=''>State</option>
+          <option key='State'value=''>All States</option>
           {setFilters('state')}
         </select>
       </div>
@@ -76,7 +86,11 @@ export const mapStateToProps = (state) => ({
   events: state.events,
 });
 
-export default connect(mapStateToProps)(Filters);
+export const mapDispatchToProps = (dispatch) => ({
+  getEvents: (type, state, month) => dispatch(getEvents(type, state, month)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filters);
 
 Filters.propTypes = {
   events: PropTypes.array
