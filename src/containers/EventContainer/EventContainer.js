ca310042841151c01cf1e8a21019e8f62425659d
Whitney Burton <whitneyburton9@gmail.com>
2019-04-08 19:17:04 -0600

    Add logic to render correct cards for EventContainer on Profile tabs

--- src/containers/EventContainer/EventContainer.js
+++ src/containers/EventContainer/EventContainer.js
@@ -3,13 +3,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import EventCard from '../../components/EventCard/EventCard';
 
-export const EventContainer = ({ pathname, events }) => {
-  const styles = pathname.includes('profile') ? 'EventContainer profile' : 'EventContainer home';
-    return (
-      <div className={styles}>
-      {
-        events.map(event => 
-          <EventCard 
+export const EventContainer = ({ pathname, events, userEvents }) => {
+  const allEvents = events.map(event => (
+    <EventCard
+      id={event.id}
+      key={event.id}
+      name={event.attributes.name}
+      image={event.attributes.image_url}
+      city={event.attributes.city}
+      state={event.attributes.state}
+      date={event.attributes.start_date}
+      pathname={pathname}
+    />
+  ));
+
+  const tabOptions = ['attending', 'wishlist', 'past'];
+  const tabEvents = tabOptions.map(option => {
+    return userEvents
+      .filter(event => event.attributes.status === `${option}`)
+      .map(event => {
+        return (
+          <EventCard
             id={event.id}
             key={event.id}
             name={event.attributes.name}
@@ -19,18 +33,33 @@ export const EventContainer = ({ pathname, events }) => {
             date={event.attributes.start_date}
             pathname={pathname}
           />
-        )
-      }
-      </div>
-    )
-}
+        );
+      });
+  });
 
-export const mapStateToProps = (state) => ({
+  const styles = pathname.includes('profile')
+    ? 'EventContainer profile'
+    : 'EventContainer home';
+
+  if (pathname.includes('upcoming')) {
+    return <div className={styles}>{tabEvents[0]}</div>;
+  } else if (pathname.includes('wishlist')) {
+    return <div className={styles}>{tabEvents[1]}</div>;
+  } else if (pathname.includes('past')) {
+    return <div className={styles}>{tabEvents[2]}</div>;
+  } else {
+    return <div className={styles}>{allEvents}</div>;
+  }
+};
+
+export const mapStateToProps = state => ({
   events: state.events,
+  userEvents: state.userEvents
 });
 
 export default connect(mapStateToProps)(EventContainer);
 
 EventContainer.propTypes = {
-  events: PropTypes.array
-}
+  events: PropTypes.array,
+  userEvents: PropTypes.array
+};
