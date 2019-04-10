1129c2488c2c7f52f73e8199aedeacf7e637b06c
Karin Ohman <kaohma@gmail.com>
2019-04-09 19:47:25 -0600

    Add event container tests

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -141,3 +141,46 @@ export const mockUser = {
   image_url: 'https://alumni.turing.io/sites/default/files/styles/graduate_full_profile/public/squareheadshot1.jpg?itok=EGlR-8YR',
   bio: 'Hi I’m Jane and I have no friends because this app only has one user, and it’s me!!! Hi I’m Jane and I have no friends because this app only has one user, and it’s me!!! Hi I’m Jane and I have no friends because this app only has one user, and it’s me!!! Hi I’m Jane and I have no friends because this app only has one user, and it’s me!!!'
 }
+
+export const mockUserEvents = [
+  {
+    id: "6",
+    type: "event",
+    attributes: {
+      name: "Leavenworth Rock Fest",
+      city: "Canon Springs",
+      state: "CO",
+      event_type: "climbing",
+      price: 35,
+      start_date: "10-19-2018",
+      end_date: "10-21-2018",
+      description:
+        "This October 19-21, head on down to the best limestone sport cragging Colorado has to offer, and with the American Alpine Club and CAMP USA, and be treated to a weekend of climbing, community, clinics, contests, beer, slideshows, films, giveaways, stewardship and fun.",
+      event_url: "https://americanalpineclub.org/shelf-road-cc",
+      image_url:
+        "https://static1.squarespace.com/static/55830fd9e4b0ec758c892f81/57bc7284f7e0abe0efbb523e/59417dc21b631bad37b19a74/1497464437416/_MG_1604.jpg",
+      video_url: "https://www.youtube.com/embed/Cnjv_Brs2Bo",
+      status: 'attending'
+    }
+  },
+  {
+    id: "3",
+    type: "event",
+    attributes: {
+      name: "Joes Valley Fest",
+      city: "Canon Springs",
+      state: "CO",
+      event_type: "climbing",
+      price: 35,
+      start_date: "10-19-2018",
+      end_date: "10-21-2018",
+      description:
+        "This October 19-21, head on down to the best limestone sport cragging Colorado has to offer, and with the American Alpine Club and CAMP USA, and be treated to a weekend of climbing, community, clinics, contests, beer, slideshows, films, giveaways, stewardship and fun.",
+      event_url: "https://americanalpineclub.org/shelf-road-cc",
+      image_url:
+        "https://static1.squarespace.com/static/55830fd9e4b0ec758c892f81/57bc7284f7e0abe0efbb523e/59417dc21b631bad37b19a74/1497464437416/_MG_1604.jpg",
+      video_url: "https://www.youtube.com/embed/Cnjv_Brs2Bo",
+      status: 'wishlist'
+    }
+  }
+]
