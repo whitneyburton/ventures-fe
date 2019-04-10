9e13e313dcd983857beaaae1fa5a24c213efd0bc
Karin Ohman <kaohma@gmail.com>
2019-04-09 19:36:32 -0600

    Add profile tests

--- src/_tests_/Profile.test.js
+++ src/_tests_/Profile.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Profile, mapDispatchToProps } from '../containers/Profile/Profile';
+import { Profile, mapStateToProps, mapDispatchToProps } from '../containers/Profile/Profile';
 import { setUser } from '../actions';
+import { updateUser } from '../thunks/updateUser';
 import { mockUser } from '../data/mockData';
+jest.mock('../thunks/updateUser');
 
 const mockProps = {
   setUser: jest.fn(),
@@ -22,12 +24,35 @@ describe('Profile', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  describe('mapStateToProps', () => {
+    it('should return a props object with events', () => {
+      const mockState = {
+        user: { name: 'Wonderwoman', bio: 'About me'},
+        isLoading: true,
+        error: ''
+      };
+      const expectedState = {
+        user: { name: 'Wonderwoman', bio: 'About me' },
+      };
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps).toEqual(expectedState);
+    });
+  });
+
   describe('mapDispatchToProps', () => {
     it('should call dispatch with the setUser action', () => {
       const mockDispatch = jest.fn();
       const actionToDispatch = setUser();
       const mappedProps = mapDispatchToProps(mockDispatch);
       mappedProps.setUser();
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should call dispatch with the updateUser action', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = updateUser();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.updateUser();
       expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
     });
   });
