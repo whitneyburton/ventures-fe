8588c4aa330fc049e05a5a0e1eb274e081e8a7bb
Karin Ohman <kaohma@gmail.com>
2019-04-09 19:53:28 -0600

    Add event pop up tests

--- src/_tests_/EventPopUp.test.js
+++ src/_tests_/EventPopUp.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { EventPopUp, mapDispatchToProps } from '../containers/EventPopUp/EventPopUp';
+import { EventPopUp, mapStateToProps, mapDispatchToProps } from '../containers/EventPopUp/EventPopUp';
+import { mockUserEvents } from '../data/mockData';
 import { getEvent } from '../thunks/getEvent';
+import { changeUserEvent } from '../thunks/changeUserEvent';
+import { getUserEvents } from '../thunks/getUserEvents';
 jest.mock('../thunks/getEvent');
+jest.mock('../thunks/changeUserEvent');
+jest.mock('../thunks/getUserEvents');
 
 const mockProps = {
+  changeUserEvent,
+  getUserEvents,
   getEvent,
+  userEvents: mockUserEvents,
   match: { params: { id: '1' } },
   history: { goBack: jest.fn() }
 }
@@ -21,12 +29,43 @@ describe('EventPopUp', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  describe('mapStateToProps', () => {
+    it('should return a props object with events', () => {
+      const mockState = {
+        userEvents: mockUserEvents,
+        isLoading: true,
+        error: ''
+      };
+      const expectedState = {
+        userEvents: mockUserEvents
+      };
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps).toEqual(expectedState);
+    });
+  });
+
   describe('mapDispatchToProps', () => {
     it('should call dispatch with the getEvent thunk', async () => {
       const mockDispatch = jest.fn();
       const actionToDispatch = getEvent('1');
       const mappedProps = mapDispatchToProps(mockDispatch);
       mappedProps.getEvent('1');
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should call dispatch with the changeUserEvent thunk', async () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = changeUserEvent('1', 'POST', 'attending');
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.changeUserEvent('1', 'POST', 'attending');
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+
+    it('should call dispatch with the getUserEvents thunk', async () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = getUserEvents('1');
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.getUserEvents('1');
       expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
     });
   });
