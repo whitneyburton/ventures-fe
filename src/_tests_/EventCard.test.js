64c908adb7442a23e2d658d7042e9d409121aaf9
Karin Ohman <kaohma@gmail.com>
2019-04-10 16:39:45 -0600

    Add event card snapshot tests

--- src/_tests_/EventCard.test.js
+++ src/_tests_/EventCard.test.js
@@ -3,25 +3,36 @@ import { shallow } from 'enzyme';
 import EventCard from '../components/EventCard/EventCard';
 import { mockEvents } from '../data/mockData';
 
-const mockProps = {
-  id: mockEvents.data[0].id,
-  key: mockEvents.data[0].id,
-  name: mockEvents.data[0].attributes.name,
-  image: mockEvents.data[0].attributes.image_url,
-  city: mockEvents.data[0].attributes.city,
-  state: mockEvents.data[0].attributes.state,
-  date: mockEvents.data[0].attributes.start_date,
-  pathname: '/'
-}
 
 describe('EventCard', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<EventCard {...mockProps} />)
+  
+  it('should match the snapshot from the home path', () => {
+    const mockProps = {
+      id: mockEvents.data[0].id,
+      key: mockEvents.data[0].id,
+      name: mockEvents.data[0].attributes.name,
+      image: mockEvents.data[0].attributes.image_url,
+      city: mockEvents.data[0].attributes.city,
+      state: mockEvents.data[0].attributes.state,
+      date: mockEvents.data[0].attributes.start_date,
+      pathname: '/'
+    };
+    const wrapper = shallow(<EventCard {...mockProps} />);
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it('should match the snapshot', () => {
+  it('should match the snapshot from the profile path', () => {
+    const mockProps = {
+      id: mockEvents.data[0].id,
+      key: mockEvents.data[0].id,
+      name: mockEvents.data[0].attributes.name,
+      image: mockEvents.data[0].attributes.image_url,
+      city: mockEvents.data[0].attributes.city,
+      state: mockEvents.data[0].attributes.state,
+      date: mockEvents.data[0].attributes.start_date,
+      pathname: '/profile'
+    };
+    const wrapper = shallow(<EventCard {...mockProps} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
