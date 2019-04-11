2a4c0209b4f18799f6336699d40a57930b0a92c8
Whitney Burton <whitneyburton9@gmail.com>
2019-04-10 19:05:03 -0600

    Add dynamic profileStyles to NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export const NavBar = () => {
+export const NavBar = ({ pathname }) => {
+  const profileStyles = pathname.includes('profile') ? 'profile-active' : 'profile-button';
+
   return (
     <div className='NavBar'>
       <NavLink to='/'><h2 className='ventures-title'>Ventures</h2></NavLink>
       <p className='ventures-slogan'>A place to find and track outdoor events that speak to you.</p>
-      <NavLink to='/' className='home-button' />
-      <NavLink to='/profile/upcoming' className='profile-button' />
+      <NavLink exact to='/' className='home-button' activeClassName='home-active' />
+      <NavLink to='/profile/upcoming' className={profileStyles} />
     </div>
   )
 }
