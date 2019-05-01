fcf77269dbc6bf33601440664768989a2866e5c8
Karin Ohman <kaohma@gmail.com>
2019-05-01 16:20:59 -0600

    Update tests based on changes

--- src/_tests_/NavBar.test.js
+++ src/_tests_/NavBar.test.js
@@ -4,9 +4,10 @@ import NavBar from '../components/NavBar/NavBar';
 
 describe('NavBar', () => {
   let wrapper;
+  let mockUser = {name: 'Wonder Woman', bio: 'bio here'}
 
   beforeEach(() => {
-    wrapper = shallow(<NavBar pathname={'/'}/>)
+    wrapper = shallow(<NavBar pathname={'/'} user={mockUser}/>)
   });
 
   it('should match the snapshot', () => {
