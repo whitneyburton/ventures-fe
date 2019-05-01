6cec84baea16af3d640c2d189548ea38ad550f57
Karin Ohman <kaohma@gmail.com>
2019-05-01 14:55:16 -0600

    Add login component and initial styling

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,39 @@
+import React, { useState, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../../thunks/getUser';
+import { getUserEvents } from '../../thunks/getUserEvents';
+import { connect } from 'react-redux';
+
+export const Login = ({ getUser, getUserEvents, history }) => {
+  const [email, updateEmail] = useState('');
+  const [password, updatePassword] = useState('');
+
+  const handleSubmit = () => {
+    getUser('1');
+    getUserEvents('1');
+    history.replace('/profile/upcoming');
+  }
+
+  return(
+    <Fragment>
+      <div className='overlay'></div>
+      <form autoComplete='off' onSubmit={handleSubmit} className='Login'>
+        <label>Email
+              <input onChange={(e) => updateEmail(e.target.value)} required type='email' placeholder='Enter your email' id='email' value={email} />
+        </label>
+        <label>Password
+              <input onChange={(e) => updatePassword(e.target.value)} required type='password' placeholder='Enter your password' id='password' value={password} />
+        </label>
+        <button type='submit'>Submit</button>
+        <Link className='pop-up-link' to='/sign-up'>Sign Up Here</Link>
+      </form>
+    </Fragment>
+  )
+}
+
+export const mapDispatchToProps = (dispatch) => ({
+  getUser: (id) => dispatch(getUser(id)),
+  getUserEvents: (id) => dispatch(getUserEvents(id))
+});
+
+export default connect(null, mapDispatchToProps)(Login);
