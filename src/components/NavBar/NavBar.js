373da3bf63a38a3b8436f83ebde4117666de070e
Karin Ohman <kaohma@gmail.com>
2019-05-01 14:16:53 -0600

    Add conditional to profile button for login

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export const NavBar = () => {
+export const NavBar = ({ user }) => {
   return (
     <div className='NavBar'>
       <NavLink to='/'><h2 className='ventures-title'>Ventures</h2></NavLink>
       <p className='ventures-slogan'>A place to find and track outdoor events that speak to you.</p>
       <NavLink exact to='/' className='home-button' activeClassName='home-active' />
-      <NavLink to='/profile/upcoming' className='profile-button' activeClassName='profile-active'/>
+      <NavLink 
+        to={user.name ? '/profile/upcoming' : '/login'}
+        className='profile-button' 
+        activeClassName='profile-active' />
     </div>
   )
 }
