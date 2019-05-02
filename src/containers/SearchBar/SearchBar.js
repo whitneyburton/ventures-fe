21965057a7c612513f7a9a099bfb21f979a618cd
Karin Ohman <kaohma@gmail.com>
2019-05-02 11:35:11 -0600

    Remove autocomplete from search input

--- src/containers/SearchBar/SearchBar.js
+++ src/containers/SearchBar/SearchBar.js
@@ -9,7 +9,7 @@ export const SearchBar = ({ setSearchText, searchText}) => {
       <h3 className='searchbar-title'>SEARCH EVENTS</h3>
       <div className='searchbar-container'>
         <img src={searchIcon} alt='search icon' className='searchbar-icon' />
-        <input onChange={(e) => setSearchText(e.target.value)} name='searchQuery' placeholder='Search all events' className='searchbar-input' value={searchText}/>
+        <input autocomplete='off' onChange={(e) => setSearchText(e.target.value)} name='searchQuery' placeholder='Search all events' className='searchbar-input' value={searchText}/>
       </div>
     </div>
   );
